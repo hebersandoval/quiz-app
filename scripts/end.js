c806b76d22433e0ddec01898628d2cd0be4bf56f
Heber Sandoval <he3ers@gmail.com>
2022-05-14 18:33:37 -0400

    QUIZ-10 Can save name of players and their score in localStorage.

--- scripts/end.js
+++ scripts/end.js
@@ -2,6 +2,11 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('save-score-btn');
 const finalScore = document.getElementById('final-score');
 
+const MAX_HIGH_SCORES = 5;
+
+// Get high score or return an empty []
+highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
@@ -12,5 +17,23 @@ username.addEventListener('keyup', (event) => {
 
 const saveHighScore = (event) => {
     event.preventDefault();
-    console.log('Clicked on save');
+
+    const score = {
+        // score: mostRecentScore,
+        score: Math.floor(Math.random() * 100),
+        name: username.value,
+    };
+    // Add score to the []
+    highScores.push(score);
+    // Keep top 5 high score
+    highScores.sort((a, b) => {
+        // If b is higher than a, then put b before a
+        return b.score - a.score;
+    });
+    // At index 5, start cutting everything
+    highScores.splice(5);
+    // Save to localStorage
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    // Go back home
+    window.location.assign('/');
 };
